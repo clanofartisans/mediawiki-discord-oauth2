5cd4c5d80564418d25e02bb5366c987dad0bd166
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2023-11-03 22:38:44 -0700

    Update SpecialOAuth2Client.php

--- SpecialOAuth2Client.php
+++ SpecialOAuth2Client.php
@@ -177,6 +177,7 @@ class SpecialOAuth2Client extends SpecialPage {
 		}
 
 		$username = ucfirst(JsonHelper::extractValue($response, $wgOAuth2Client['configuration']['username']));
+		$username = str_replace( '_', '', $username);
 		$realname = (JsonHelper::extractValue($response, $wgOAuth2Client['configuration']['username']) . '#' . (JsonHelper::extractValue($response, $wgOAuth2Client['configuration']['discriminator'])));
 		$email = JsonHelper::extractValue($response, $wgOAuth2Client['configuration']['email']);
 		Hooks::run("OAuth2ClientBeforeUserSave", [&$username, &$email, $response]);
